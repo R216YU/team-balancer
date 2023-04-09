d2748bb278d983a539f4443ba1972557acdaa34a
R216YU <ryu0216sp@gmail.com>
2023-04-09 16:27:40 +0900

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         notoSans: ["Noto Sans JP", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
+      colors: {
+        back: "#111827",
+        backHover: "#374151",
+        str: "#E5E7EB",
+        accent: "#0ED3CF",
+        accentHover: "#0E9B9D",
+      },
     },
   },
   plugins: [],
